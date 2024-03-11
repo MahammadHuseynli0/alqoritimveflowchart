b2b43d377de679fa43345744045bc74b4de01db0
MahammadHuseynli0 <tu7uv5kks.@code.edu.az>
2024-03-11 11:10:49 +0400

    tapsiriq

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("ededi daxil ele");
+            //int a = (int)Convert.ToInt64(Console.ReadLine());
+            //int qaliq;
+            //int result = 1;
+            //while (a > 0)
+            //{
+            //    qaliq = a % 10;
+            //    a /= 10;
+            //    result *= qaliq;
+            //}
+            //Console.WriteLine(result);
+        }
+    }
+}
+
+
+
